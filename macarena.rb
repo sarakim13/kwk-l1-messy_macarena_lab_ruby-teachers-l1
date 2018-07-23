7f775c53a08757af703ec81d9457031fb525b508
Sara Kim <sarakim0903@gmail.com>
2018-07-23 20:36:34 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -6,7 +6,7 @@ puts ""
 # The first instruction is correct. 
 
 # Step 1: GOOD
-puts "Step 1: Right arm out in front of you, palm facing down."
+puts "Right arm out in front of you, palm facing down."
 
 # Step 2: FIXME
 puts "Left arm out in front of you, palm facing down."
